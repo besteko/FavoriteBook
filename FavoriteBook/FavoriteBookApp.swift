0d0b08bd008bf99f25323e8ffd4d41edba2a39ae
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-06-22 12:37:29 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Beste Kocaoglu on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
